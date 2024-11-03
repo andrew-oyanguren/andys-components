e4decd2a22d8246679d00992406f1b93b3b815d2
Andrew Oyanguren <andy@Andrews-MacBook-Pro.local>
2024-11-03 15:06:54 -0800

    Added Loader component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import Card from './components/Card';
 import Button from './components/Button';
+import Loader from './components/Loader';
 
 function App() {
   return (
     <div className="App">
+      <p style={{marginTop: 20}}>Card</p>
       <Card>
         <h1>Mastering React</h1>
       </Card>
-      
+      <p style={{marginTop: 20}}>Button</p>
       <Button title="Click me" onClick={() => console.log('Button clicked')} />
+      <p style={{marginTop: 20}}>Loader</p>
+      <Loader />
     </div>
   );
 }
---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,9 @@
+import styles from './index.module.css';
+
+export default function Loader() {
+  return  (
+    <div className={styles.container}>
+      <p>Loading...</p>
+    </div>
+  );
+};
